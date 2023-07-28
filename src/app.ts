80262f08a5905bbe7fc9227c9133c6df7f622237
guirms <guilhermesantana84@hotmail.com>
2023-07-28 00:14:55 -0300

    feat: changing project to typescript

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+// import express from 'express';
+
+// export class App {
+//     private express: express.Application;
+//     private port = 3000;
+
+//     constructor() {
+//         this.express = express();
+//         this.listen();
+//     }
+
+//     public getApp(): express.Application {
+//         return this.express;
+//     }
+
+
+//     private listen(): void {
+//         this.express.listen(this.port, () => {
+//             console.log('Servidor iniciado na porta ' + this.port);
+//         });
+//     }
+// }
+
+console.log('teste')
