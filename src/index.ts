ada95e994da107924d17dd0bb334db935552abc5
Guilherme Machado Santana <guilherme.santana@plasson.com.br>
2023-07-28 13:45:15 -0300

    feat: creating route for api call

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,16 @@
-import express from 'express';
 import { config } from 'dotenv';
-import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
-import { GetUsersController } from './controllers/get-users/get-users';
+import express from 'express';
+import { userRoute } from './routes/user-route';
 
 config();
 
-const app = express()
+const app = express();
 
 const port = process.env.PORT || 8000;
+
+app.use('/user', userRoute);
 
 app.listen(port, () => {
     console.log('listening on port ' + port);
 });
 
-app.get("/users", async (req, res) => {
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
-
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
-
-    const { body, statusCode } = await getUsersController.handle();
-
-    res.status(statusCode).send(body);
-  });
